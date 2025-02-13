356a5e8b5383c8f3e56c344a96e8203c1c905130
ariane00 <83496684+ariane00@users.noreply.github.com>
2025-02-13 14:55:28 -0500

    Updated Dockerfile and bot.py

--- BotTemplate/BotCode/bot.py
+++ BotTemplate/BotCode/bot.py
@@ -2,10 +2,26 @@ from abc_classes import ABot
 from teams_classes import NewUser, NewPost
 import random
 import string
+import os
 import json
+from openai import OpenAI
+
+API_KEY = os.getenv('ENV_VAR1')
+client = OpenAI(api_key=API_KEY)
+
+
 from datetime import datetime, timedelta
 
 class Bot(ABot):
+    def create_tweetGPT(self, keyword):
+            response = client.chat.completions.create(model="gpt-4",
+            messages=[
+                {"role": "system", "content": "You are a chronically online twitter person who is writing tweets"},
+                {"role": "user", "content": f"Write a short tweet that has a 50% chance of mentionning {keyword}"}
+            ],
+            max_tokens=50)
+            return response.choices[0].message.content
+
     def create_user(self, session_info):
         # todo logic
         self.metadata = session_info.metadata 
@@ -14,7 +30,7 @@ class Bot(ABot):
         self.end_time = getattr(session_info, "end_time", None)
 
         existing_usernames = set() #to avoid repetition
-        
+
         def username_generator():
             #prefix
             prefixes = ["lil", "theBest", "Big", "Mister", "Kween", "Dr", "Super", "Crafty", "TheReal", "Official","TheOG", "user", "Massive", "CraZ", "Baby", "The", "__", "_","our","__.", "the", "aa."]  #add randomly
@@ -27,11 +43,11 @@ class Bot(ABot):
             ("Paul", "Pauly"),("Emily", "Em"), ("Andrew", "Drew"), ("Donna", "Donnie"), ("Joshua", "Josh"), ("Michelle", "Mitch"),("Kenneth", "Ken"), ("Dorothy", "Dot"), ("Kevin", "Kev"), ("Carol", "Carrie"), ("Brian", "Bri"),("Amanda", "Mandy"), ("George", "Georgie"), ("Melissa", "Mel"), ("Edward", "Ed"), ("Deborah", "Debbie")]
             funny_nicknames = [
             ("James", "Jimbo"), ("Mary", "Moo"), ("John", "J-Dog"), ("Patricia", "Trishy"), ("Robert", "Robby-Bob"),("Jennifer", "Jelly"), ("Michael", "Mikey Mike"), ("Linda", "L-Dawg"), ("William", "Billy the Kid"), ("Elizabeth", "Lizzie McGuire"),("David", "D-Money"), ("Barbara", "Babs"), ("Richard", "Dicky"), ("Susan", "Sushi"), ("Joseph", "JoJo"),("Jessica", "Jessinator"), ("Thomas", "Tommy Pickles"), ("Sarah", "Sazzy"), ("Charles", "Chuckles"), ("Karen", "K-Ren"),
-            ("Christopher", "Topher"), ("Nancy", "Nanners"), ("Daniel", "D-Man"), ("Lisa", "Lizard"), ("Matthew", "Matty Ice"),("Betty", "Betz"), ("Anthony", "Ant-Man"), ("Margaret", "Megatron"), ("Mark", "Marky Mark"), ("Sandra", "Sandy Cheeks"),("Donald", "Don Juan"), ("Ashley", "Ash Ketchum"), ("Steven", "Stevie Wonder"), ("Kimberly", "Kiki"),("Paul", "P-Diddy"), ("Emily", "Em&Em"), ("Andrew", "Drewbie"), ("Donna", "DonDon"), ("Joshua", "Juicy J"),("Michelle", "Mitchie"), ("Kenneth", "Kenny G"), ("Dorothy", "DotDot"), ("Kevin", "Kevlar"), ("Carol", "Carrot"),("Brian", "Bri-Bri"), ("Amanda", "Mandarin"), ("George", "Georgio"), ("Melissa", "Melon"), ("Edward", "Eddie Spaghetti"),("Deborah", "Debo"), ("Franklin", "Frank the Tank"), ("Tiffany", "Tiff-Tiff"), ("Oscar", "Ozzy"), ("Lucas", "Lukey Duke"),
+            ("Christopher", "Topher"), ("Nancy", "Nanners"), ("Daniel", "D-Man"), ("Lisa", "Lizard"), ("Matthew", "Matty Ice"),("Betty", "Betz"), ("Anthony", "Ant-Man"), ("Margaret", "Megatron"), ("Mark", "Marky Mark"), ("Sandra", "Sandy Cheeks"),("Donald", "Don Juan"), ("Ashley", "Ash Ketchum"), ("Steven", "Stevie Wonder"), ("Kimberly", "Kiki"), ("Emily", "Em&Em"), ("Andrew", "Drewbie"), ("Donna", "DonDon"), ("Joshua", "Juicy J"),("Michelle", "Mitchie"), ("Kenneth", "Kenny G"), ("Dorothy", "DotDot"), ("Kevin", "Kevlar"), ("Carol", "Carrot"),("Brian", "Bri-Bri"), ("Amanda", "Mandarin"), ("George", "Georgio"), ("Melissa", "Melon"), ("Edward", "Eddie Spaghetti"),("Deborah", "Debo"), ("Franklin", "Frank the Tank"), ("Tiffany", "Tiff-Tiff"), ("Oscar", "Ozzy"), ("Lucas", "Lukey Duke"),
             ("Victoria", "VeeVee"), ("Theodore", "Teddy Bear"), ("Cynthia", "Cyn"), ("Leonard", "Lenny Face"), ("Phillip", "Flip"),("Veronica", "Ronnie"), ("Samuel", "Samwise"), ("Nicole", "Nikki Minaj"), ("Gregory", "Greggles"), ("Raymond", "Ray-Ray"),("Harold", "Harry-O"), ("Gerald", "G-Money"), ("Sylvia", "Sylvester"), ("Walter", "Wally World"), ("Isabella", "Izzy Bizzy"),("Xavier", "X-Man"), ("Felicia", "Bye Felicia"), ("Brandon", "B-Rad"), ("Catherine", "Kitty"), ("Leon", "Leo the Lion"),("Randy", "Randog"), ("Vincent", "Vinnie the Pooh"), ("Eugene", "Gene Machine"), ("Monica", "Momo"), ("Charlotte", "Charizard")
     ]
 
-            
+
             use_funny_name = random.choice([True, False])
             real_name, nickname = random.choice(funny_nicknames if use_funny_name else normal_nicknames)
 
@@ -40,7 +56,7 @@ class Bot(ABot):
             num_digits = random.randint(0, 4) #random digits
             suffix = ''.join(random.choices(string.digits, k=num_digits))  
             username = f"{nickname}{suffix}"
-            
+
             #20% of the time add a prefix if there are available ones
             if random.random() < 0.2 and prefixes:
                 available_prefixes = list(set(prefixes) - used_prefixes) 
@@ -53,7 +69,7 @@ class Bot(ABot):
                 existing_usernames.add(username)
                 name = real_name if random.random() < 0.7 else nickname  # Sometimes use real name, sometimes nickname
                 return username, name
-        
+
         user_descriptions = [user["description"] for user in session_info.users if user.get("description")]
 
         # Example:
@@ -61,7 +77,7 @@ class Bot(ABot):
         NewUser(username=user[0], name=user[1], description=random.choice(user_descriptions) if user_descriptions else "Hello, I'm a bot")
         for user in [username_generator() for _ in range(10)]
         ]
-        
+
         return new_users
 
     def generate_content(self, datasets_json, users_list):
@@ -78,7 +94,6 @@ class Bot(ABot):
                 topic_keywords = [kw for topic in topics if isinstance(topic, dict) for kw in topic.get("keywords", [])]
                 influence_target = random.choice(topic_keywords) if topic_keywords else "random_trending"
 
-        #print("Extracted Keywords:", topic_keywords) 
 
         existing_posts = [post["text"] for post in getattr(datasets_json, "posts", []) if "text" in post]
 
@@ -98,10 +113,14 @@ class Bot(ABot):
                 post_time = session_start + timedelta(seconds=random_offset)
                 post_time_str = post_time.strftime("%Y-%m-%dT%H:%M:%S.000Z")
 
-                if existing_posts and random.random() < 0.85:
+                choice_type = random.random()
+                if existing_posts and choice_type < 0.6:
                     text = random.choice(existing_posts)
+                elif choice_type < 0.3:
+                    keyword = influence_target if random.random() < 0.5 else random.choice(topic_keywords) 
+                    text = self.create_tweetGPT(keyword)
                 else:
-                    keyword = influence_target if random.random() < 0.5 else random.choice(topic_keywords) if topic_keywords else "NO_VALID_TOPICS"
+                    keyword = influence_target if random.random() < 0.5 else random.choice(topic_keywords) 
                     text = random.choice([
                         f"Can't stop thinking about {keyword}.",f"Why is everyone talking about {keyword}?",f"Anyone else obsessed with {keyword} lately?",f"{keyword} is taking over my life!",f"What are your thoughts on {keyword}?",f"Just saw something wild about {keyword}.",f"Let’s settle this: is {keyword} overrated?",f"Can’t believe what just happened in {keyword}!",
                         f"My feed is full of {keyword}, and I’m not mad about it.",f"Lowkey addicted to {keyword}.",f"If you don’t love {keyword}, we can’t be friends.",f"Does anyone still care about {keyword}?",f"Need more {keyword} content ASAP.",
